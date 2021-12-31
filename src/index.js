ac71f08c967f5d705b05de173ae5422d88f4afb1
PranayChavhan <pranay21chavhan@outlook.com>
2021-12-31 16:53:06 +0530

    Heading changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App heading='Convert Lowercase to Uppercase'/>
+    <App heading='Lowercase Uppercase Conversion'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
